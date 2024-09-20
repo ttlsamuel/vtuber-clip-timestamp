02dc8b0085edee15619f45f2af9f5f756e237778
ttlsamuel <ttlsamuel@gmail.com>
2024-09-20 22:22:55 +0800

    Update timestamp_functions.py

--- timestamp_functions.py
+++ timestamp_functions.py
@@ -5,7 +5,6 @@ import subprocess
 fps         = "fps=5"
 resolution  = "320x240"
 
-
 # read file name from directories with one item
 def name_extract(path):
     filename = str(os.listdir(path)[0])
@@ -17,13 +16,9 @@ def cleaning(path):
     for filename in os.listdir(path):
         os.remove(os.path.join(path, filename))
 
-
-# the following codes only run when this py file is being executed
-if __name__ == "__main__":
-
-    # executing reconfiguration of raw vod
-    cleaning(r'C:\Users\user\Desktop\video timestamp project\ffmpeg vod')
-    raw_vod_name = (name_extract(r"C:\Users\user\Desktop\video timestamp project\raw vod") )
-    subprocess.run([ "ffmpeg", "-i"  , raw_vod_name
-                        , "-filter:v",  fps , "-s", resolution, "-preset",  "ultrafast"
-                        , r"C:\Users\user\Desktop\video timestamp project\ffmpeg vod\Reconfig_vod.mp4"], shell=True)
+# executing reconfiguration of raw vod
+cleaning(r'C:\Users\user\Desktop\video timestamp project\ffmpeg vod')
+raw_vod_name = (name_extract(r"C:\Users\user\Desktop\video timestamp project\raw vod") )
+subprocess.run([ "ffmpeg", "-i"  , raw_vod_name
+                    , "-filter:v",  fps , "-s", resolution, "-preset",  "ultrafast"
+                    , r"C:\Users\user\Desktop\video timestamp project\ffmpeg vod\Reconfig_vod.mp4"], shell=True)
