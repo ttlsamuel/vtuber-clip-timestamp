9ce79c118474b8816d93be92f972b7137f06ce1b
ttlsamuel <ttlsamuel@gmail.com>
2024-09-21 03:17:28 +0800

    Update reconfig.py

--- reconfig.py
+++ reconfig.py
@@ -5,10 +5,10 @@ import subprocess
 fps         = "fps=5"
 resolution  = "320x240"
 
-
-# executing reconfiguration of raw vod
-cleaning(r'C:\Users\user\Desktop\video timestamp project\ffmpeg vod')
-raw_vod_name = (name_extract(r"C:\Users\user\Desktop\video timestamp project\raw vod") )
-subprocess.run([ "ffmpeg", "-i"  , raw_vod_name
-                    , "-filter:v",  fps , "-s", resolution, "-preset",  "ultrafast"
-                    , r"C:\Users\user\Desktop\video timestamp project\ffmpeg vod\Reconfig_vod.mp4"], shell=True)
+def reconfigure():
+    # executing reconfiguration of raw vod
+    cleaning(r'C:\Users\user\Desktop\video timestamp project\ffmpeg vod')
+    raw_vod_name = (name_extract(r"C:\Users\user\Desktop\video timestamp project\raw vod") )
+    subprocess.run([ "ffmpeg", "-i"  , raw_vod_name
+                        , "-filter:v",  fps , "-s", resolution, "-preset",  "ultrafast"
+                        , r"C:\Users\user\Desktop\video timestamp project\ffmpeg vod\Reconfig_vod.mp4"], shell=True)
