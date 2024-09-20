6651d58f053b1fcc8f2c47ef36ea34cc30a9a418
ttlsamuel <ttlsamuel@gmail.com>
2024-09-20 23:48:02 +0800

    Update and rename cleaning_machine.py to clean_all.py

---
+++ clean_all.py
@@ -0,0 +1,5 @@
+from file_func import cleaning
+
+cleaning(r'C:\Users\user\Desktop\video timestamp project\clip img')
+cleaning(r'C:\Users\user\Desktop\video timestamp project\ffmpeg frame')
+cleaning(r'C:\Users\user\Desktop\video timestamp project\raw vod')
