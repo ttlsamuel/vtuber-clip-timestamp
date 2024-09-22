2ae657fe181c3e4732833e846b3e52c4527b4149
ttlsamuel <ttlsamuel@gmail.com>
2024-09-22 21:54:07 +0800

    Update yt_dlp_dl.py

--- yt_dlp_dl.py
+++ yt_dlp_dl.py
@@ -8,8 +8,12 @@ def yt_download():
     subprocess.run(["yt-dlp", "-F", yt_link])
 
     dl_format = input( "\n" + "Choose your download format by entering the corresponding id: ")
-    subprocess.run(["yt-dlp", "--output" , r"C:\Users\user\Desktop\video timestamp project\raw vod\raw vod.mp4"
+    subprocess.run(["yt-dlp", "--output" , r"C:\Users\user\Desktop\video timestamp project\raw vod\%(title)s.%(ext)s"
                        , "--embed-thumbnail", "-f", dl_format, "--merge-output-format", "mp4" , yt_link])
 
     end_time = datetime.now()
     print('Runtime: {}'.format(end_time - start_time) + "\n")
+
+
+if __name__ == '__main__':
+    yt_download()
