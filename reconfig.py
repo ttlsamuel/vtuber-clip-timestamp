903c00cce6c8a4fcbc4ea261a2f4d634eafd3b7a
ttlsamuel <ttlsamuel@gmail.com>
2024-09-22 21:55:47 +0800

    Update reconfig.py

--- reconfig.py
+++ reconfig.py
@@ -1,5 +1,6 @@
 from file_func import name_extract, cleaning
 import subprocess
+import os
 
 # adjust reconfig_vid metadata here
 fps         = "fps=5"
@@ -11,4 +12,11 @@ def reconfigure():
     raw_vod_name = (name_extract(r"C:\Users\user\Desktop\video timestamp project\raw vod") )
     subprocess.run([ "ffmpeg", "-i"  , raw_vod_name
                         , "-filter:v",  fps , "-s", resolution, "-preset",  "ultrafast"
-                        , r"C:\Users\user\Desktop\video timestamp project\ffmpeg vod\Reconfig_vod.mp4"], shell=True)
+                        , r"C:\Users\user\Desktop\video timestamp project\ffmpeg vod\reconfig_vod.mp4"], shell=True)
+    os.rename(r'C:\Users\user\Desktop\video timestamp project\ffmpeg vod\reconfig_vod.mp4',
+              r'C:\Users\user\Desktop\video timestamp project\ffmpeg vod\\' + 'RECONFIG ' +
+              os.listdir(r"C:\Users\user\Desktop\video timestamp project\raw vod")[0] )
+
+
+if __name__ == "__main__":
+    reconfigure()
