85bcb8c857b100d1b8dac26c2c10eb33a67171fb
ttlsamuel <ttlsamuel@gmail.com>
2024-09-27 19:30:52 +0800

    Update clean_all.py

--- clean_all.py
+++ clean_all.py
@@ -1,5 +1,6 @@
 from file_func import cleaning
 
 cleaning(r'C:\Users\user\Desktop\video timestamp project\clip img')
-cleaning(r'C:\Users\user\Desktop\video timestamp project\ffmpeg frame')
+cleaning(r'C:\Users\user\Desktop\video timestamp project\ffmpeg vod')
 cleaning(r'C:\Users\user\Desktop\video timestamp project\raw vod')
+cleaning(r"C:\Users\user\Desktop\video timestamp project\clip audio")
