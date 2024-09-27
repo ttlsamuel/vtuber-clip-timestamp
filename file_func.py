457ffd975702c6fee8c1a31f2e7e2d940440296f
ttlsamuel <ttlsamuel@gmail.com>
2024-09-27 18:25:53 +0800

    Update file_func.py

--- file_func.py
+++ file_func.py
@@ -10,3 +10,9 @@ def name_extract(path):
 def cleaning(path):
     for filename in os.listdir(path):
         os.remove(os.path.join(path, filename))
+
+def time_output(timestamp):
+    time_amount = timestamp; time_amount = time_amount % (24 * 3600); hour = time_amount // 3600
+    time_amount %= 3600; minutes = time_amount // 60; time_amount %= 60; seconds = time_amount
+    print("The approximate timestamp is: " + str(int(hour))
+          + " hour(s) " + str(int(minutes)) + " minute(s) " + str("{:.1f}".format(seconds)) + " second(s).")
